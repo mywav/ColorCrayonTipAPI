8f78f8d7ef013e004184fa46e084cca7ebe4d1f5
mywav <ryan_newbold@me.com>
2020-05-24 14:43:11 -0600

    Made some changes to AppConfig.java

--- src/main/java/com/thevisualcommunicationguy/config/AppConfig.java
+++ src/main/java/com/thevisualcommunicationguy/config/AppConfig.java
@@ -1,6 +1,5 @@
 package com.thevisualcommunicationguy.config;
 
-
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
@@ -15,31 +14,33 @@ import org.springframework.web.context.support.XmlWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
-public class AppConfig implements WebApplicationInitializer{
+public class AppConfig implements WebApplicationInitializer {
 //	@Bean(name = "entityManagerFactory")
 //	public LocalEntityManagerFactoryBean entityManagerFactory(){
 //	     LocalEntityManagerFactoryBean factoryBean = new LocalEntityManagerFactoryBean();
 //	     factoryBean.setPersistenceUnitName("quiz-persistence-unit");
 //	     return factoryBean;
 //	}
-	
+
 	@Bean
 	public DataSource dataSource() {
 		DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-		dataSourceBuilder.driverClassName("org.sqlite.JDBC");
-		dataSourceBuilder.url("jdbc:sqlite:colorcrayontip.db");
+		dataSourceBuilder.driverClassName("org.postgresql.Driver");
+		dataSourceBuilder.url(
+				"postgres://mzyxfkgazradhd:da8088a6cf4657d729b56fddadccfcf44b38f9a0a826a64396ccf66495c17ec2@ec2-52-44-166-58.compute-1.amazonaws.com:5432/debsupspc92meh");
+		dataSourceBuilder.username("mzyxfkgazradhd");
+		dataSourceBuilder.password("da8088a6cf4657d729b56fddadccfcf44b38f9a0a826a64396ccf66495c17ec2");
 		return dataSourceBuilder.build();
 	}
 
 	@Override
 	public void onStartup(ServletContext container) throws ServletException {
 		XmlWebApplicationContext context = new XmlWebApplicationContext();
-        context.setConfigLocation("/WEB-INF/spring/dispatcher-config.xml");
- 
-        ServletRegistration.Dynamic dispatcher = container
-          .addServlet("dispatcher", new DispatcherServlet(context));
- 
-        dispatcher.setLoadOnStartup(1);
-        dispatcher.addMapping("/");		
+		context.setConfigLocation("/WEB-INF/spring/dispatcher-config.xml");
+
+		ServletRegistration.Dynamic dispatcher = container.addServlet("dispatcher", new DispatcherServlet(context));
+
+		dispatcher.setLoadOnStartup(1);
+		dispatcher.addMapping("/");
 	}
 }
