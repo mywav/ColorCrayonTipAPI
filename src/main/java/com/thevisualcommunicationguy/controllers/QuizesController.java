18fdb6b893e1b78f2e65efda6257af62ccb88e1f
mywav <ryan_newbold@me.com>
2020-06-07 21:31:02 -0600

    Added quiz.getUsername() to quizes controller

--- src/main/java/com/thevisualcommunicationguy/controllers/QuizesController.java
+++ src/main/java/com/thevisualcommunicationguy/controllers/QuizesController.java
@@ -70,7 +70,7 @@ public class QuizesController {
 			Connection connection = dataSource.getConnection();
 			Statement statement = connection.createStatement();
 			statement.execute(
-					"INSERT INTO quizes (username) VALUES ('" + quiz.getUsername() + "');");
+					"INSERT INTO quizes (username, nameofpaper) VALUES ('" + quiz.getUsername() + ", " + quiz.getUsername() + "');");
 			statement.close();
 			connection.close();
 		} catch (SQLException e) {
